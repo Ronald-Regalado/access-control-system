26c84d5c5468f7677c176b7ff96dff343769f38e
Ronald Regalado Batista <regaladoronald83@gmail.com>
2024-09-01 00:05:52 -0400

    Configuración de las solicitudes http de los servicios en el programa principal

--- AccessControlSystem.GrpcServices/Program.cs
+++ AccessControlSystem.GrpcServices/Program.cs
@@ -20,6 +20,9 @@ namespace AccessControlSystem.GrpcServices
             app.MapGrpcService<GreeterService>();
             app.MapGrpcService<UnitService>();
             app.MapGrpcService<UserService>();
+            app.MapGrpcService<UserScheduleService>();
+            app.MapGrpcService<UserSessionService>();
+
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
